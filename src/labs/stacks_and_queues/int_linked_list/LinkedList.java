eff9f298be29881b79e47bf5de418d279707af2c
Michael DeSanty <michael.desanty@gmail.com>
2024-02-26 20:34:26 -0500

    Stacks and Queues lab stuffs

---
+++ src/labs/stacks_and_queues/int_linked_list/LinkedList.java
@@ -0,0 +1,56 @@
+package labs.stacks_and_queues.int_linked_list;
+
+public class LinkedList {
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+    }
+
+    public void append(int data) {
+
+    }
+
+    public void prepend(int data) {
+
+    }
+
+    public int getHead() {
+
+    }
+
+    public int getTail() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void removeByValue(int data) {
+
+    }
+
+    public int removeHead() {
+
+    }
+
+    public int removeTail() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    private class Node {
+        public Node next;
+        public int data;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
