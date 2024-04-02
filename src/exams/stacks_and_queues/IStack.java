2f4bb37951d689b0777b99117d4115392cb68645
Michael DeSanty <michael.desanty@gmail.com>
2024-04-02 13:10:09 -0400

    Stacks and queues exam

---
+++ src/exams/stacks_and_queues/IStack.java
@@ -0,0 +1,13 @@
+package exams.stacks_and_queues;
+
+public interface IStack<T> {
+    public void push(T item);
+
+    public T pop();
+
+    public T peek();
+
+    public int size();
+
+    public boolean isEmpty();
+}
---
+++ src/exams/stacks_and_queues/StackLL.java
@@ -0,0 +1,36 @@
+package exams.stacks_and_queues;
+
+import java.util.LinkedList;
+
+public class StackLL<T extends Comparable<T>> implements IStack<T> {
+    private LinkedList<T> items;
+
+    public StackLL() {
+        items = new LinkedList<>();
+    }
+
+    @Override
+    public void push(T item) {
+        items.addFirst(item); // addFirst is the equivalent to append
+    }
+
+    @Override
+    public T pop() {
+        return items.poll(); // poll is the equivalent to removeHead
+    }
+
+    @Override
+    public T peek() {
+        return items.peek(); // peek is the equivalent to getHead
+    }
+
+    @Override
+    public int size() {
+        return items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+}
