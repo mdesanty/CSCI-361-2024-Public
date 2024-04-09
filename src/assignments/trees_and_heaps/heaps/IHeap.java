cb19168ba314ad4e322080aed906d2f9e77dea38
Michael DeSanty <michael.desanty@gmail.com>
2024-04-09 00:09:05 -0400

    IHeap

---
+++ src/assignments/trees_and_heaps/heaps/IHeap.java
@@ -0,0 +1,11 @@
+package assignments.trees_and_heaps.heaps;
+
+public interface IHeap {
+    public void insert(int value);
+
+    public int remove();
+
+    public int size();
+
+    public int peek();
+}
